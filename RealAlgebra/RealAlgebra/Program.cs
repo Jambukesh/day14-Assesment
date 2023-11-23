0bde8a87c84250c25799068df26242742cb84c5c
Jambukesh <jkesh360@gmail.com>
2023-11-23 16:43:49 +0530

    day14

---
+++ RealAlgebra/RealAlgebra/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeArea;
+namespace RealAlgebra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter\n1.Square,\n2.Reactangle,\n3.Circle,\n4.Triangle");
+            int value = int.Parse(Console.ReadLine());
+            switch (value)
+            {
+                case 1:
+                    Console.WriteLine("Enter side value ");
+                    int side = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Square.Area(side));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Length value ");
+
+                    int Length = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Width value ");
+                    int Width = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Height value ");
+                    int height = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine(Rectangle.Area(Length, Width, height));
+                    break;
+                case 3:
+                    Console.WriteLine("Enter radious value ");
+                    int radius = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Circle.Area(radius));
+                    break;
+                case 4:
+                    Console.WriteLine("Enter Base value ");
+                    int Base = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Height value ");
+                    int Height = int.Parse(Console.ReadLine());
+                   
+                    Console.WriteLine(Triangle.Area(Base, Height));
+                    break;
+
+            }
+        }
+    }
+}
---
+++ ShapeArea/ShapeArea/ShapeAreaLib.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    public static class Rectangle
+    {
+        //   public double Length { get; set; }
+        //   public double Width { get; set; }
+        //  public double Height { get; set; }
+        public static double Area(int Length,int Width,int height)
+        {
+            return Length * Width * height;
+        }
+    }
+    public static class Square
+    {
+      //  public double Side { get; set; }
+        public static double Area(int Side)
+        {
+            return Side * Side;
+        }
+
+    }
+     public static class Circle
+    {
+        public static double Area(int radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    public static class Triangle
+    {
+        public static double Area(int Base, int Height)
+    {
+            return 0.5 * Base * Height;
+    }
+}
+       
+    
+
+    
+       
+
+    //public class ShapeAreaLib
+    //{
+     
+    //}
+}
